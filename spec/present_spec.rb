06295b65e0de4772eca2e0f07cbc3e716d20f70c
omar <omar-malik22@hotmail.com>
2023-01-18 23:00:04 +0000

    went over testing errors 'presents'

--- spec/present_spec.rb
+++ spec/present_spec.rb
@@ -1,11 +1,20 @@
 require 'present'
 
 RSpec.describe Present do
-  context "already wrapped contents." do
-    it "fails" do
-        present = Present.new()
-        present.wrap("h")
-        expect { present.wrap("s") }.to raise_error "A contents has already been wrapped."
-    end
+  it "wraps and unwraps value" do
+    present = Present.new
+    present.wrap("something")
+    expect(present.unwrap).to eq "something"
+  end
+
+  it "fails unwrap without wrapping first" do
+    present = Present.new
+    expect { present.unwrap }.to raise_error "No contents have been wrapped."
+  end
+
+  it "fails when trying to wrap already wrapped present" do
+    present = Present.new
+    present.wrap("something")
+    expect { present.wrap("another thing") }.to raise_error "A contents has already been wrapped."
   end
 end
