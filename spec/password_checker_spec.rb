035ed9c86942c29a87691e1422126889daa0f325
omar <omar-malik22@hotmail.com>
2023-01-19 08:54:52 +0000

    went over password checker challenge

--- spec/password_checker_spec.rb
+++ spec/password_checker_spec.rb
@@ -1,26 +1,18 @@
 require "password_checker"
 
-describe 'PasswordChecker' do
-    context "given a password below 8 letters" do
-        it "fails" do
-            password_checker = PasswordChecker.new
-            expect { password_checker.check("") }.to raise_error "Invalid password, must be 8+ characters."
-        end
+RSpec.describe PasswordChecker do
+    it "returns true if password length is > 8" do
+        password = PasswordChecker.new
+        expect(password.check("123456789")).to eq true
     end
 
-    context "given a password which is 8 letters" do
-        it "returns true" do
-            password_checker = PasswordChecker.new
-            result = password_checker.check("qwertyuio")
-            expect(result).to eq true
-        end
+    it "returns true if password length is == 8" do
+        password = PasswordChecker.new
+        expect(password.check("12345678")).to eq true
     end
 
-    context "given a password which is above 8 letters" do
-        it "returns true" do
-            password_checker = PasswordChecker.new
-            result = password_checker.check("qwertyuiopqwer")
-            expect(result).to eq true
-        end
+    it "fails when password length is < 8" do
+        password = PasswordChecker.new
+        expect { password.check("123") }.to raise_error "Invalid password, must be 8+ characters."
     end
 end
