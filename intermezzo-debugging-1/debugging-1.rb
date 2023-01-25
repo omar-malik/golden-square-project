0d8a7091fbe64fcff7ef6ab1e438fb6c13b3be8d
omar <omar-malik22@hotmail.com>
2023-01-25 01:13:00 +0000

    finished debugging 1

--- intermezzo-debugging-1/debugging-1.rb
+++ intermezzo-debugging-1/debugging-1.rb
@@ -13,10 +13,20 @@ end
 def decode(ciphertext, key)
   cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
   plaintext_chars = ciphertext.chars.map do |char|
-    cipher[65 - char.ord]
+    cipher[char.ord - 65]
   end
   return plaintext_chars.join
 end
 
-puts encode("theswiftfoxjumpedoverthelazydog", "secretkey")
-puts decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
+p encode("theswiftfoxjumpedoverthelazydog", "secretkey")
+p decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
+
+def get_most_common_letter(text)
+  counter = Hash.new(0)
+  text.chars.each do |char|
+    counter[char] += 1
+  end
+  counter.to_a.sort_by { |k, v| v }[-1][0]
+end
+
+p get_most_common_letter("sometthing")
